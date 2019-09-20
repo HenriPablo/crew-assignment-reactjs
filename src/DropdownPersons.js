8f466c0d8575df299beafa7983e495fa268749e7
tomekpilot@gmail.com <nov99344>
2019-09-20 17:58:15 -0400

    interim - roles and persons drop-downs refactor

--- src/DropdownPersons.js
+++ src/DropdownPersons.js
@@ -64,29 +64,20 @@ const connectedDropdownSelect = connect(
 export const DropdownPersons = connectedDropdownSelect(
     class extends Component {
         buildDropdownOptions() {
-            //console.log("props in DROPDOWN PERSONS:", this.props);
-            //let x = {}; this was just a transfer object when we had to iterte over the KEYs
+
             let y = [];
 
-            //console.log("this.props.ass.length: ", this.props.ass.length);
-            //console.log("this.props.ass: ", this.props.ass)
-
+            //console.log("Build PERSONS Dropdown Options")
             /** add default person to dropdown by preference value */
-            console.log("this.props.preferences.alwaysRenderSelf.value: ", this.props.preferences.alwaysRenderSelf.value);
-            console.log("this.props.personsKey: ", this.props.personsKey );
-            console.log("this.props.personsKey === 0: ", this.props.personsKey === 0 );
-            console.log("this.props.ass[0].assignedPerson !== null: ", this.props.ass[0].assignedPerson !== null);
-            console.log("this.props.ass[0].assignmentKey: ", this.props.ass[0].assignmentKey);
+            //console.log("this.props.preferences.alwaysRenderSelf.value: ", this.props.preferences.alwaysRenderSelf.value);
+            //console.log("this.props.personsKey: ", this.props.personsKey );
+            //console.log("this.props.personsKey === 0: ", this.props.personsKey === 0 );
+            //console.log("this.props.ass[0].assignedPerson !== null: ", this.props.ass[0].assignedPerson !== null);
+            //console.log("this.props.ass[0].assignmentKey: ", this.props.ass[0].assignmentKey);
 
             if(
                 /** initoal app load */
                 this.props.preferences.alwaysRenderSelf.value === true && this.props.ass.length === 1
-                || /** subsequent renders */
-                (
-                    this.props.preferences.alwaysRenderSelf.value === true && this.props.ass[0].assignmentKey === 0 //this.props.personsKey === 0
-                    //&&
-                    //this.props.ass[0].assignedPerson !== null
-                )
             )
             {
                 console.log("1st IF");
@@ -96,68 +87,54 @@ export const DropdownPersons = connectedDropdownSelect(
                     >{this.props.ass[0].assignedPerson.first_name} {this.props.ass[0].assignedPerson.last_name}</option>);
             }
 
-            else if( this.props.ass[0].assignmentKey === 0  /*&& this.props.ass[0].assignedPerson !== null .self === "true" */ ){
-                console.log("3rd IF");
-                y.push(<option key={this.props.ass[0].assignedPerson.id} value={this.props.ass[0].assignedPerson.id}>{this.props.ass[0].assignedPerson.first_name}</option>);
-            }
-
             else
             {
-                console.log("1st ELSE");
-                console.log("props BEFORE loop in persons: ", this.props.ass )
+                //console.log( "\n1st ELSE - props BEFORE loop in persons: ", this.props.ass )
 
                 for (let i = 0; i < this.props.ass.length; i++) {
-                    console.log("1st LOOP");
-                    console.log("LOOP COUNT: ", i );
+                    //console.log("1st LOOP");
+                    //console.log("LOOP COUNT: ", i );
                     //console.log("typeof this.props.ass[i].assignedPersons: ", typeof this.props.ass[i].assignedPersons);
-                    //console.log("this.props.ass[i].assignedPersons: ", this.props.ass[i].assignedPersons);
+                    console.log("LOOP COUNT: " + i + " this.props.ass[" + i + "]: ", this.props.ass[i]);
 
 
-                    console.log("this.props.ass[i].assignedPerson !== null: ", this.props.ass[i].assignedPerson !== null)
-                    console.log("this.props.ass[i].assignedPerson: ", this.props.ass[i].assignedPerson )
-                    console.log( "this.props.ass[i].assignmentKey: ", this.props.ass[i].assignmentKey );
-                    if (
-                   //     this.props.ass[i].assignmentKey !== 0 &&
+                    //console.log("this.props.ass[i].assignedPerson !== null: ", this.props.ass[i].assignedPerson !== null)
+                    //console.log("this.props.ass[i].assignedPerson: ", this.props.ass[i].assignedPerson )
+                    //console.log( "this.props.ass[i].assignmentKey: ", this.props.ass[i].assignmentKey );
+
+                    //console.log('typeof this.props.ass[i].assignedPersons !== "undefined": ', typeof this.props.ass[i].assignedPersons !== "undefined")
+                    //else
+                        if (
+                    //    this.props.ass[i].assignmentKey > 0 &&
                    //     this.props.ass[i].assignmentKey === this.props.personsKey &&
-                        typeof  this.props.ass[i].assignedPersons !== "undefined"
-                   //     && this.props.ass[i].assignedPersons !== null
-                   //     && this.props.ass[i].assignedPerson !== "true"
+                   //     this.props.ass[i].assignedPersons !== null
+                        //&& this.props.ass[i].assignedPersons !== null
+                    //    && this.props.ass[i].assignedPerson !== null
+                    //    && this.props.ass[i].assignedPerson.self !== "true"
+                           typeof this.props.ass[i].assignedPersons !== "undefined"
                     )
                     {
-                        console.log("2ND IF");
-                        console.log("props in 2ND IF: ", this.props)
-                        //x = this.props.ass[i].assignedPersons;
+                        //console.log("2ND IF");
+                        //console.log("props in 2ND IF: ", this.props)
 
+                        /** THS RUNS WHEN SELECTING A PERSON AFTER ROLE SELECTION AFTER ASSIGN BUTTON CLICK */
                         for( let ii = 0; ii < this.props.ass[i].assignedPersons.length; ii++ ){
-                            y.push(<option
-                                key={this.props.ass[i].assignedPersons[ii].id}
-                                value={this.props.ass[i].assignedPersons[ii].id}
-                            >{this.props.ass[i].assignedPersons[ii].first_name} {this.props.ass[i].assignedPersons[ii].last_name}</option>);
+
+                            console.log("2ND LOOP: this.props.ass[i].assignmentKey: ", this.props.ass[i].assignmentKey)
+
+                                y.push(<option
+                                    key={this.props.ass[i].assignedPersons[ii].id}
+                                    value={this.props.ass[i].assignedPersons[ii].id}
+                                >{this.props.ass[i].assignedPersons[ii].first_name} {this.props.ass[i].assignedPersons[ii].last_name}</option>);
                         }
-
                     }
-
-                    if (this.props.ass[i].assignedPerson !== null &&  this.props.ass[i].assignmentKey == 0 ){
+                    else
+                        if ( this.props.ass[i].assignmentKey === 0  && this.props.ass[i].assignedPerson !== null){
                         console.log('ELSE IF 5')
-                       // alert('hi')
                         y.push(<option key={this.props.ass[i].assignedPerson.id} value={this.props.ass[i].assignedPerson.id}>{this.props.ass[i].assignedPerson.first_name}</option>);
-
                     }
-
 
                 }
-
-                //if (typeof x !== "undefined") {
-                    // console.log("3rd IF");
-                    //
-                    // console.log("X in typeof x !== \"undefined\" in PERSONS: ",  x );
-                    // Object.keys( x ).forEach(item => {
-                    //     console.log("ITEM: ", item)
-                    //    y.push(<option key={x.id} value={x.id}>{x.first_name}</option>);
-                    //});
-
-                    //for( )
-                //}
             } // end else
 
 
