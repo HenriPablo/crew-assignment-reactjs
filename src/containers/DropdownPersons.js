39502202578a079dda86e165493901fbd5e21f9b
tomekpilot@gmail.com <nov99344>
2019-11-12 18:01:02 -0500

    clean up refactoring

--- src/containers/DropdownPersons.js
+++ src/containers/DropdownPersons.js
@@ -48,14 +48,14 @@ export const DropdownPersons = connectedDropdownSelect(
 
             let y = [];
 
-            let makeOption = (key, value, label) =>{
-                return <option key={key} value={value}>{label}</option>
+            let makeOption = (idValue, label) =>{
+                return <option key={idValue} value={idValue}>{label}</option>
             }
 
             console.log( "this.props at top of buildDropdownOptions: ", this.props );
             /** add default person to dropdown by preference value */
             if(
-                /** initoal app load */
+                /** initial app load */
                 this.props.preferences.alwaysRenderSelf.value === true &&
                 this.props.ass.length === 1
                 && this.props.ass[0].assignedPerson !== null
@@ -66,14 +66,11 @@ export const DropdownPersons = connectedDropdownSelect(
             )
             {
                 console.log("1st IF");
-                y.push( makeOption(this.props.ass[0].assignedPerson.id, this.props.ass[0].assignedPerson.id, (this.props.ass[0].assignedPerson.first_name + " " + this.props.ass[0].assignedPerson.last_name) )
-
-                    // <option
-                    // key={this.props.ass[0].assignedPerson.id}
-                    // value={this.props.ass[0].assignedPerson.id}
-                    // >{this.props.ass[0].assignedPerson.first_name} {this.props.ass[0].assignedPerson.last_name}
-                    // </option>
-
+                y.push(
+                    makeOption(
+                        assignments[0].assignedPerson.id,
+                        (assignments[0].assignedPerson.first_name + " " + assignments[0].assignedPerson.last_name)
+                    )
                 );
             }
             /** Changing default role on a default assignment */
@@ -83,15 +80,15 @@ export const DropdownPersons = connectedDropdownSelect(
                 this.props.preferences.alwaysRenderSelf.defaultRole !== this.props.ass[0].assignedRole
             ){
                 console.log("1st ESLE IF")
-                let p1 = this.props;
-                let a1 = this.props.ass;
-                for( let i1 = 0; i1 < a1.length; i1++ )
+                for( let i1 = 0; i1 < assignments.length; i1++ )
                 {
-                    for( let ii = 0; ii < a1[i1].assignedPersons.length; ii++ ){
-                        y.push(<option
-                            key={this.props.ass[i1].assignedPersons[ii].id}
-                            value={this.props.ass[i1].assignedPersons[ii].id}
-                        >{this.props.ass[i1].assignedPersons[ii].first_name} {this.props.ass[i1].assignedPersons[ii].last_name}</option>);
+                    for( let ii = 0; ii < assignments[i1].assignedPersons.length; ii++ ){
+                        y.push(
+                            makeOption(
+                                assignments[i1].assignedPersons[ii].id,
+                                (assignments[i1].assignedPersons[ii].first_name + " " + assignments[i1].assignedPersons[ii].last_name)
+                            )
+                        )
                     }
                 }
 
@@ -99,8 +96,6 @@ export const DropdownPersons = connectedDropdownSelect(
             else
             {
                 console.log(" in 'ELSE' BEFORE LOOP props: ", this.props );
-                //let p = this.props;
-                //let a = this.props.ass;
                 for (let i = 0; i < assignments.length; i++) {
 
                     if( props.preferences.alwaysRenderSelf.value === true &&
@@ -111,11 +106,11 @@ export const DropdownPersons = connectedDropdownSelect(
                     {
                         console.log(" in 2nd 'IF' INSIDE LOOP p: ", props );
                         y.push(
-                            <option
-                                key={this.props.ass[i].assignedPerson.id}
-                                value={this.props.ass[i].assignedPerson.id}>
-                                {this.props.ass[i].assignedPerson.first_name} {this.props.ass[i].assignedPerson.last_name}
-                            </option>);
+                            makeOption(
+                                assignments[i].assignedPerson.id,
+                                (assignments[i].assignedPerson.first_name + " " + assignments[i].assignedPerson.last_name)
+                            )
+                        )
                     }
 
                     else if
@@ -136,10 +131,12 @@ export const DropdownPersons = connectedDropdownSelect(
                         if( props.personsKey === assignments[i].assignmentKey){
                             console.log(" in 3rd 'IF' INSIDE LOOP A: ", assignments );
                             for( let ii = 0; ii < assignments[i].assignedPersons.length; ii++ ){
-                                y.push(<option
-                                    key={this.props.ass[i].assignedPersons[ii].id}
-                                    value={this.props.ass[i].assignedPersons[ii].id}
-                                >{this.props.ass[i].assignedPersons[ii].first_name} {this.props.ass[i].assignedPersons[ii].last_name}</option>);
+                                y.push(
+                                    makeOption(
+                                        assignments[i].assignedPersons[ii].id,
+                                        (assignments[i].assignedPersons[ii].first_name + " " + assignments[i].assignedPersons[ii].last_name)
+                                    )
+                                )
                             }
                         }
                     }
@@ -155,10 +152,12 @@ export const DropdownPersons = connectedDropdownSelect(
                     {
                         console.log(" in LAST  'ELSE-IF' INSIDE LOOP A: ", assignments );
                         for( let ii = 0; ii < assignments[i].assignedPersons.length; ii++ ){
-                            y.push(<option
-                                key={this.props.ass[i].assignedPersons[ii].id}
-                                value={this.props.ass[i].assignedPersons[ii].id}
-                            >{this.props.ass[i].assignedPersons[ii].first_name} {this.props.ass[i].assignedPersons[ii].last_name}</option>);
+                            y.push(
+                                makeOption(
+                                    assignments[i].assignedPersons[ii].id,
+                                    (assignments[i].assignedPersons[ii].first_name + " " + assignments[i].assignedPersons[ii].last_name)
+                                )
+                            )
                         }
                     }
                 }// end for loop
