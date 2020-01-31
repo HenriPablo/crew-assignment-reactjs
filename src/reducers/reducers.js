a73b5c402062d908dbfc3e760f597f7e7fbf7b8e
tomekpilot@gmail.com <nov99344>
2020-01-31 12:39:15 -0500

    clean up

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -3,78 +3,10 @@
 import persons from "../data/people2.json";
 import plane from "../data/plane";
 import messages from "../data/messages";
-//import preferences from "../data/preferences"
+
 
 const counter = (state, action) => {
-    //console.log("state: ", state);
     console.log("action in REDUCER: ", action);
-
-    // let getDefaultPerson = function(){
-    //     //return preferences.alwaysRenderSelf.defaultPerson; //persons.self;
-    //     for( let i = 0; i < persons.length; i++ ){
-    //         console.log( "reducer persons[i]: ", persons[i]);
-    //         if( persons[i].self === "true" ){
-    //             console.log( "reducer persons[i]: ", persons[i]);
-    //             return persons[i];
-    //         }
-    //     }
-    // }
-
-
-    // let defaultAssigment = function(x){
-    //     let defaultAss = [];
-    //     if( typeof x != "undefined" && x.alwaysRenderSelf.value === true){
-    //         //console.log('trying to create a deafult assignment')
-    //         console.log("getDefaultPerson(x): ", getDefaultPerson(x) );
-    //         defaultAss = [{
-    //             "assignedPerson": getDefaultPerson(x),
-    //             "assignedPersons": getDefaultPerson(x),// { [preferences.alwaysRenderSelf.defaultPerson] : persons.self },
-    //             "assignedRole": x.alwaysRenderSelf.defaultRole, //getDefaultRole(x),
-    //             "assignmentKey": 0
-    //         }]
-    //     }
-    //     console.log("defaultAss in DEFAULTASSIGMENT() in reducer");
-    //     console.log(defaultAss)
-    //     return defaultAss;
-    //
-    // }
-
-    /**
-    let getAssigned = function(x){
-        if( typeof x != "undefined" && x.alwaysRenderSelf.value === true){
-            return 1;
-        } else { return 0; }
-    }
-
-    // advance the nextKey if we're rendering a default assignment with pre-selected values on the first page load
-    let getNextKey = function(x){
-        if( typeof x != "undefined" && x.alwaysRenderSelf.value === true){
-            return 1;
-        } else {
-            return  0;
-        }
-    }
-
-    let getCount = function(x){
-        if( typeof x != "undefined" && x.alwaysRenderSelf.value === true ){
-            return 1;
-        } else { return 0 }
-    }
-
-    let getA = function (a1, a2, a3 ) {
-        let a = [];
-        if(typeof a1 != "undefined" && a1 != null ){ //&& a1.size > 0
-            a.push( a1);
-        }
-        if(typeof a2 != "undefined" &&  a2 !=null ){
-            a.push( a2);
-        }
-        if(typeof a3 != "undefined" &&  a3 != null && a3.size > 0){
-            a.push( a3 );
-        }
-        return  a;
-    }
-     */
 
     if (state === undefined) {
         return {
@@ -95,25 +27,12 @@ const counter = (state, action) => {
         }
     }
 
-
-
     switch (action.type) {
 
-        // case "START_AJAX_INIT":
-        //      console.log("hi from START_AJAX_INIT in Reducer");
-        //      console.log( "Action in START_AJAX_INIT reducer: ", action );
-        //      return{
-        //          ...state
-        //      }
-
         case "AJAX_INIT_DONE":
-             // console.log("hi from AJAX_INIT_DONE in Reducer");
-              console.log( "in CASE - Action in AJAX_INIT_DONE reducer: ", action );
-             console.log("ASS length in AJAX_INIT_DONE: ", action.bigJ.ass.length )
               return{
                  ...state,
                  preferences: action.bigJ.preferences,
-                 //ass:  [action.bigJ.ass, ...state.ass],
                   ass: action.bigJ.ass,
                  count: action.bigJ.count,
                  nextKey: action.bigJ.nextKey,
@@ -123,22 +42,15 @@ const counter = (state, action) => {
 
         /** THIS ONE NEEDS TO GET PREFERENCES AND ROLES */
         case "assign":
-            //console.log("jsonPreferences in reducer - ASSIGN", action.jsonPreferences)
 
-            let z =
-             {
+             return {
                 ...state,
-                //preferences: action.jsonPreferences,
                 count: state.count + 1,
                 nextKey: state.count + 1,
-                //ass: getA(action.ass, ...state.ass, defaultAssigment(action.jsonPreferences) ),
                  ass: [ action.ass, ...state.ass],
                  assigned: state.assigned + 1,
                 showModal: state.showModal
             }
-            //console.log("z in ASSIGN action:");
-            //console.log( z );
-            return z;
 
         case 'GET_ROLES' :
             return {
@@ -152,7 +64,6 @@ const counter = (state, action) => {
                 ...state,
                 roles: action.json,
                 loading: false
-                //preferences: action.jsonPreferences
             };
 
         case 'PEOPLE_RECEIVED' :
--- src/sagas/DoAjaxInitSaga.js
+++ src/sagas/DoAjaxInitSaga.js
@@ -1,26 +1,19 @@
-import {put, takeLatest, takeEvery} from "@redux-saga/core/effects";
-//export {triggerInitAjaxDataActionWatcher}
+import {put, takeLatest} from "@redux-saga/core/effects";
 
 let getDefaultPerson = function(persons){
     let defautlPerson = {};
-    //return preferences.alwaysRenderSelf.defaultPerson; //persons.self;
     for( let i = 0; i < persons.length; i++ ){
-        //console.log( "SAGA persons[i]: ", persons[i]);
         if( persons[i].self === "true" ){
-            //console.log( "AJAX_INIT_DONE SAGA persons[i]: ", persons[i]);
             defautlPerson = persons[i];
             break;
         }
     }
-    console.log( "Deafault Perosn in Saga: ", defautlPerson );
     return defautlPerson;
 }
 
 let defaultAssigment = function( prefs, people ){
     let defaultAss = [];
     if( typeof prefs != "undefined" && prefs.alwaysRenderSelf.value === true){
-        //console.log('trying to create a deafult assignment')
-        console.log("AJAX_INIT_DONE getDefaultPerson(x): ", getDefaultPerson(people) );
         defaultAss = [{
             "assignedPerson": getDefaultPerson(people),
             "assignedPersons": getDefaultPerson(people),// { [preferences.alwaysRenderSelf.defaultPerson] : persons.self },
@@ -45,7 +38,7 @@ function* workFetchInitAjaxData( action ) {
 
     const jsonPreferences = {
         "alwaysRenderSelf" : {
-            "value" : true,
+            "value" : false,
             "tip" : "Render dropdown selects for Self as pilot and specified role, for example PIC, Student, etc.",
             "defaultRole" : "59",
             "defaultPerson" : "self"
@@ -69,8 +62,6 @@ function* workFetchInitAjaxData( action ) {
         const peopleJson = yield fetch('http://localhost:3000/ajax-people.json?roleId=89' , {headers : {'Content-Type': 'application/json','Accept': 'application/json'}})
             .then( response => response.json());
 
-        console.log("peopleJson in AJAX_INIT_DONE SAGA: ", peopleJson )
-//        action.ass.push( defaultAssigment( jsonPreferences, peopleJson) );
         action.ass = defaultAssigment( jsonPreferences, peopleJson);
         action.count = 1;
         action.nextKey = 1;
@@ -79,18 +70,9 @@ function* workFetchInitAjaxData( action ) {
 
     let bigJ = action;
 
-    //console.log("action AJAX_INIT_DONE SAGA: ", action);
-
-    //jsonPreferences: jsonPreferences,
     yield put( { type: "AJAX_INIT_DONE", bigJ } );
 }
 
-export function* triggerInitAjaxDataActionWatcher(action) {
-
-    // working example:
-    //https://stackblitz.com/edit/react-redux-saga-demo
-
-    //console.log("ARGUMENTS IN call to START_AJAX_INIT in triggerInitAjaxDataActionWatcher(): ", arguments)
+export function* triggerInitAjaxDataActionWatcher() {
     yield takeLatest("START_AJAX_INIT" , workFetchInitAjaxData);
-    //yield put( { type: "AJAX_INIT_DONE", action:action} );
 }
